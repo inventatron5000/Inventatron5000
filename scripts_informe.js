f6f54702cc8989d370657b54be019eb9d73ce010
Norma11AgCu <norma_aguilarc3@hotmail.com>
2017-11-14 21:42:12 -0600

    cambios informes

---
+++ scripts_informe.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+			$('#ventas').hide(); //muestro mediante id
+			$('#proveedor').hide(); //muestro mediante clase
+            $('#pisoYdepartamento').hide(); //muestro mediante clase
+    $("#VerVentas").on( "click", function() {
+			$('#ventas').hide(); //oculto mediante id
+		});
+     
+});

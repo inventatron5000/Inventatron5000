bc07299c0323519e3f6840465bf5ea499612a0ea
Jorge Garcia <14121463@tecmor.mx>
2017-11-17 12:42:23 -0600

    Git hizo un cagadero en Informe pero ya lo corregi.

--- scripts_informe.js
+++ scripts_informe.js
@@ -39,44 +39,4 @@ $(document).ready(function(){
         $("#BuscarVentas").modal("open");
     });
 
-    $('#ventas').hide(); //muestro mediante id
-	$('#proveedor').hide(); //muestro mediante clase
-    $('#pisoYdepartamento').hide(); //muestro mediante clase
-    $("#VerVentas").on( "click", function() {
-<<<<<<< HEAD
-            $("#BuscarVentas").modal("close");
-            $('#proveedor').hide(); //muestro mediante clase
-            $('#pisoYdepartamento').hide(); //muestro mediante clase
-			$('#ventas').show(); //oculto mediante id 
-		});
-     $("#VerProveedor").on( "click", function() {
-            $("#BuscarProveedor").modal("close");
-            $('#ventas').hide(); //muestro mediante id
-            $('#pisoYdepartamento').hide(); //muestro mediante clase
-			$('#proveedor').show(); //oculto mediante id   
-		})
-    $("#VerPiso").on( "click", function() {
-            $("#BuscarPiso").modal("close");
-            $('#ventas').hide(); //muestro mediante id
-            $('#proveedor').hide(); //oculto mediante id   
-            $('#pisoYdepartamento').show(); //muestro mediante clase
-		})
-    $(".VerGeneral").on( "click", function() {
-            $('#ventas').hide(); //muestro mediante id
-			$('#proveedor').hide(); //muestro mediante clase
-            $('#pisoYdepartamento').hide(); //muestro mediante clase
-            $("ul.tabs").("tab-select","#VerGeneral");
-		})
-    $("#VerPedidos").on( "click", function() {
-            $('#ventas').hide(); //muestro mediante id
-			$('#proveedor').hide(); //muestro mediante clase
-            $('#pisoYdepartamento').hide(); //muestro mediante clase
-		})
-
 });
-=======
-	   $('#ventas').hide(); //oculto mediante id
-    });
-     
-});
->>>>>>> 72bf568315cbf72ceef89596b660aeba52080cb3

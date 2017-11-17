72bf568315cbf72ceef89596b660aeba52080cb3
Jorge Garcia <14121463@tecmor.mx>
2017-11-16 22:23:19 -0600

    Correcciones al abrir modals, cambiar de tab etc. Me largo al maldito cinemex a ver JL :D

--- scripts_informe.js
+++ scripts_informe.js
@@ -1,9 +1,49 @@
 $(document).ready(function(){
-			$('#ventas').hide(); //muestro mediante id
-			$('#proveedor').hide(); //muestro mediante clase
-            $('#pisoYdepartamento').hide(); //muestro mediante clase
+    $("#btnPisoYDepartamentoFAB").click(function(evt){
+        $("#contenidoWrapper").children("div").not("div#pisoYdepartamento").hide(200);
+        $("div#pisoYdepartamento").show(200);
+    });
+    $("#btnPisoYDepartamento").click(function(evt){
+        $("#BuscarPiso").modal("open");
+    });
+
+
+    $("#btnPedidosPendientesFAB").click(function(evt){
+        $("#contenidoWrapper").children("div").not("div#pedidosPendientes").hide(200);
+        $("div#pedidosPendientes").show(200);
+    });
+
+
+    $("#btnPisoYDepartamentoFAB").click(function(evt){
+        $("#contenidoWrapper").children("div").not("div#pisoYdepartamento").hide(200);
+        $("div#pisoYdepartamento").show(200);
+    });
+    $("#btnPisoYDepartamento").click(function(evt){
+        $("#BuscarPiso").modal("open");
+    });
+
+
+    $("#btnProveedorFAB").click(function(evt){
+        $("#contenidoWrapper").children("div").not("div#proveedor").hide(200);
+        $("div#proveedor").show(200);
+    });
+    $("#btnProveedor").click(function(evt){
+        $("#BuscarProveedor").modal("open");
+    });
+
+    $("#btnVentasFAB").click(function(evt){
+        $("#contenidoWrapper").children("div").not("div#ventas").hide(200);
+        $("div#ventas").show(200);
+    });
+    $("#btnVentas").click(function(evt){
+        $("#BuscarVentas").modal("open");
+    });
+
+    $('#ventas').hide(); //muestro mediante id
+	$('#proveedor').hide(); //muestro mediante clase
+    $('#pisoYdepartamento').hide(); //muestro mediante clase
     $("#VerVentas").on( "click", function() {
-			$('#ventas').hide(); //oculto mediante id
-		});
+	   $('#ventas').hide(); //oculto mediante id
+    });
      
 });

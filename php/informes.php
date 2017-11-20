910232989400fc48512d571bc1e8386906b36e31
Jorge Garcia <14121463@tecmor.mx>
2017-11-20 15:49:38 -0600

    me falto el php xxdxd

---
+++ php/informes.php
@@ -0,0 +1,54 @@
+<?php
+    require("db_access.php");
+    extract($_POST);
+    switch($operacion){
+        case 'G':    //Informe general
+            $q = "SELECT * FROM informegeneral";
+            $informe = array();
+            $res = pg_query($q);
+            while($row = pg_fetch_array($res))
+                array_push($res,$row);
+            echo json_encode($informe);
+        break;
+        case 'D':    //Informe por Piso y Departamento
+            $q = "SELECT * FROM informepisodepa WHERE piso = '$piso' AND departamento = '$departamento' AND area = '$area'";
+            $informe = array();
+            $res = pg_query($q);
+            while($row = pg_fetch_array($res))
+                array_push($res,$row);
+            echo json_encode($informe);
+        break;
+        case 'LP':  //Lista de pisos
+            $q = "SELECT * FROM piso";
+            $lista = array();
+            $res = pg_query($q);
+            while($row = pg_fetch_array($res))
+                array_push($lista,$row);
+            echo json_encode($lista);
+        break;
+        case "LD":  //Lista de departamentos y areas
+            $q0 = "SELECT * FROM departamento ORDER BY nombre";
+            $res0 = pg_query($q0);
+            $lista = array();
+            while($dep = pg_fetch_array($res0))
+                array_push($lista,$dep);
+            echo json_encode($lista);
+        break;
+        case "LPR":    //Lista proveedores
+            $q = "SELECT nombrepr FROM proveedor";
+            $res = pg_query($q);
+            $lista = array();
+            while($row = pg_fetch_array($res))
+                array_push($lista,$row);
+            echo json_encode($lista);
+        break;
+        case "P":   //Informe por proveedores
+            $q = "SELECT * FROM informeproveedor WHERE proveedor='$proveedor'";
+            $res = pg_query($q);
+            $informe = array();
+            while($row = pg_fetch_array($res))
+                array_push($informe,$row);
+            echo json_encode($informe);
+        break;
+    }
+?>

eea3db396ed3665b46870195c1ffd3ab44867eff
Jorge Garcia <14121463@tecmor.mx>
2017-11-19 18:42:28 -0600

    Subsistema de Clientes ya funciona por completo.

--- php/clientes.php
+++ php/clientes.php
@@ -18,7 +18,7 @@
                     echo "NOK";
             }
         break;
-        case 'C':
+        case 'C':   //Consultar un men
             $q = "SELECT * from cliente WHERE nombrecl = '$nombreCliente'";
             $res = pg_query($q);
             if($row = pg_fetch_array($res)){//Ya existe
@@ -26,6 +26,34 @@
             }
             else
                 echo "NF";
+        break;
+        case 'L':   //Listar todos alvs
+            $q = "SELECT nombrecl from cliente";
+            $res = pg_query($q);
+            $lista = array();
+            while($row = pg_fetch_array($res)){
+                array_push($lista,$row);
+            }
+            echo json_encode($lista);
+        break;
+        case 'M':   //Modificar
+            $q0 = "SELECT nombrecl FROM cliente WHERE nombrecl='$nombreCliente' AND '$id'!='$nombreCliente'";
+            $res0 = pg_query($q0);
+            if($row0 = pg_fetch_array($res0))
+                echo "NOK";
+            else{
+                $q = "UPDATE cliente SET nombrecl = '$nombreCliente',personacontactocl='$personaContacto',direccionfacturacion='$direccionFact',ciudad='$ciudad',estado='$estado',codpostal='$cp',pais='$pais',telefono='$telefono',correo='$correo' WHERE nombrecl='$id'";
+                pg_query($q);
+                echo "OK";
+            }
+        break;
+        case 'B': //Baja
+            $q0 = "DELETE FROM cliente WHERE nombrecl = '$nombreCliente'";
+            $res = pg_query($q0);
+            if($res)
+                echo "OK";
+            else
+                echo "NOK";
         break;
     }
 ?>
--- scripts_clientes.js
+++ scripts_clientes.js
@@ -1,4 +1,59 @@
 $(document).ready(function(){
+    //Mostrar clientes en la vista principal
+    mostrarClientes();
+    $("#contenidoPrincipal > div > table > tbody").delegate("tr","click",function(){
+        var nombrecl = $(this).find("td").html();
+        $.ajax({
+            url:"php/clientes.php",
+            method:"post",
+            data:{
+                operacion:'C',
+                nombreCliente:nombrecl
+            }
+        }).done(function(data){
+             var datosCliente = JSON.parse(data);
+
+                var personaContacto = datosCliente.personacontactocl;
+                var pais = datosCliente.pais;
+                var estado = datosCliente.estado;
+                var ciudad = datosCliente.ciudad;
+                var cp = datosCliente.codpostal;
+                var direccionFact = datosCliente.direccionfacturacion;
+                var telefono = datosCliente.telefono;
+                var correo = datosCliente.correo;
+
+                //Colocar datos
+                $("#mostrarCliente").find("input[name='nombreCliente']").val(nombrecl);
+                $("#mostrarCliente").find("label[for='nombreCliente']").attr("class","active");
+
+                $("#mostrarCliente").find("input[name='personaContacto']").val(personaContacto);
+                $("#mostrarCliente").find("label[for='personaContacto']").attr("class","active");
+
+                $("#mostrarCliente").find("input[name='pais']").val(pais);
+                $("#mostrarCliente").find("label[for='pais']").attr("class","active");
+
+                $("#mostrarCliente").find("input[name='estado']").val(estado);
+                $("#mostrarCliente").find("label[for='estado']").attr("class","active");
+
+                $("#mostrarCliente").find("input[name='ciudad']").val(ciudad);
+                $("#mostrarCliente").find("label[for='ciudad']").attr("class","active");
+
+                $("#mostrarCliente").find("input[name='cp']").val(cp);
+                $("#mostrarCliente").find("label[for='cp']").attr("class","active");
+
+                $("#mostrarCliente").find("textarea[name='direccionFact']").val(direccionFact);
+                $("#mostrarCliente").find("label[for='direccionFact']").attr("class","active");
+
+                $("#mostrarCliente").find("input[name='telefono']").val(telefono);
+                $("#mostrarCliente").find("label[for='telefono']").attr("class","active");
+
+                $("#mostrarCliente").find("input[name='correo']").val(correo);
+                $("#mostrarCliente").find("label[for='correo']").attr("class","active");
+
+                //Cerrar ste modal y mostrar e lotro
+                $("#mostrarCliente").modal("open");
+        });
+    });
     //Funcion para agregar un nuevo piso
     $("#form-agregarCliente").submit(function(data){
 
@@ -32,8 +87,10 @@ $(document).ready(function(){
         }).done(function(data){
             if(data=="EXISTE")
                 Materialize.toast("¡Ya existe ese cliente!",2000,"rounded");
-            else if(data=="OK")
+            else if(data=="OK"){
                 Materialize.toast(str,2000,"rounded");
+                mostrarClientes();
+            }
             else
                 Materialize.toast("Error inesperado",2000,"rounded");
         });
@@ -79,6 +136,7 @@ $(document).ready(function(){
                 var telefono = datosCliente.telefono;
                 var correo = datosCliente.correo;
 
+                //Colocar datos
                 $("#editarClienteForm").find("input[name='nombreCliente']").val(nombreCliente);
                 $("#editarClienteForm").find("label[for='nombreCliente']").attr("class","active");
 
@@ -108,13 +166,15 @@ $(document).ready(function(){
 
                 //Cerrar ste modal y mostrar e lotro
                 $("#editarCliente").modal("close");
+                $("#form-editarCliente").attr("data-id",nombreCliente);
                 $("#editarClienteForm").modal("open");
             }
         });
         return false;
     });
-    //Editar un piso
+    //Editar un cliente
     $("#form-editarCliente").submit(function(evt){
+        var id= $(this).attr("data-id");
         var nombreCliente = $(this).find("input[name='nombreCliente']").val();
         var personaContacto = $(this).find("input[name='personaContacto']").val();
         var pais = $(this).find("input[name='pais']").val();
@@ -125,33 +185,102 @@ $(document).ready(function(){
         var telefono = $(this).find("input[name='telefono']").val();
         var correo = $(this).find("input[name='correo']").val();
 
-        //AQUI VA LA FUNCION QUE CONECTA CON LA DB
-        $("#editarClienteForm").modal("close");
-        var stg = "Se editó el cliente "+numeroPiso;
-        Materialize.toast(stg,2000,'rounded');
+        $.ajax({
+            url:"php/clientes.php",
+            method:"post",
+            data:{
+                operacion:'M',
+                id:id,
+                nombreCliente:nombreCliente,
+                personaContacto:personaContacto,
+                pais:pais,
+                estado:estado,
+                ciudad:ciudad,
+                cp:cp,
+                direccionFact:direccionFact,
+                telefono:telefono,
+                correo:correo
+            }
+        }).done(function(data){
+            console.log(data);
+            if(data=="NOK"){
+                Materialize.toast("El nuevo nombre de cliente ya existe",2000,"rounded");
+                $("#form-editarCliente").find("input[name='nombreCliente']").val("");
+                $("#form-editarCliente").find("input[name='nombreCliente']").focus();
+            }else if(data=="OK"){
+                var stg = "Se editó el cliente "+id;
+                Materialize.toast(stg,2000,'rounded');
+                $("#editarClienteForm").modal("close");
+                mostrarClientes();
+            }
+        });
         return false;
     });
 
     //Funcion para buscar un piso y eliminarlo
     $("#form-eliminarCliente").submit(function(evt){
-        var nombreCliente = $(this).find("input[name='nombreCliente']").val();
-        if(Math.random()<0.5){
-            $("#confirmarEliminar").find("span.nombreCliente").html(nombreCliente);
-            $("#buscarCliente").modal("close");
-            $("#confirmarEliminar").modal("open");
-        }
-        else{
-            $("#buscarCliente").modal("close");
-            var stg = "No se encontró el cliente \""+nombreCliente+"\"";
-            Materialize.toast(stg,2000,'rounded');
-        }
+        var nombrecl = $(this).find("input[name='nombreCliente']").val();
+
+        $.ajax({
+            url:"php/clientes.php",
+            method:"post",
+            data:{
+                operacion:'C',
+                nombreCliente:nombrecl
+            }
+        }).done(function(data){
+            if(data!="NF"){
+                $("#confirmarEliminar").find("span.nombreCliente").html(nombrecl);
+                $("#buscarCliente").modal("close");
+                $("#confirmarEliminar").modal("open");
+            }
+            else{
+                $("#buscarCliente").modal("close");
+                var stg = "No se encontró el cliente \""+nombreCliente+"\"";
+                Materialize.toast(stg,2000,'rounded');
+            }
+        });
+
         return false;
     });
     //Funcion para eliminar un piso
     $("#btnEliminarPiso").click(function(evt){
+        var nombrecl = $("#confirmarEliminar").find("span.nombreCliente").html();
         $("#confirmarEliminar").modal("close");
-        //FUNCION PARA BORRAR EN LA DB
-        var stg = "Se eliminó el cliente "+$("#confirmarEliminar").find("span.nombreCliente").html();
-        Materialize.toast(stg,2000,"rounded");
+        $.ajax({
+            url:"php/clientes.php",
+            method:"post",
+            data:{
+                operacion:'B',
+                nombreCliente:nombrecl
+            }
+        }).done(function(x){
+            if(x=="OK"){
+                var stg = "Se eliminó el cliente "+$("#confirmarEliminar").find("span.nombreCliente").html();
+                Materialize.toast(stg,2000,"rounded");
+                mostrarClientes();
+            }
+        });
     });
 });
+
+function mostrarClientes(){
+    $.ajax({
+        url:"php/clientes.php",
+        method:"post",
+        data:{
+            operacion:'L'
+        }
+    }).done(function(data){
+        if(data=="[]")
+            $("#contenidoPrincipal").append("No hay clientes por mostrar");
+        else{
+            var listaClientes = JSON.parse(data);
+            var tabla = "";
+            for(var k in listaClientes)
+                tabla+="<tr><td data-title='Nombre del Cliente' contenteditable='false' class='btnVerCliente'>"+listaClientes[k].nombrecl+"</td></tr>";
+            $("#contenidoPrincipal > div > table > tbody").html(tabla);
+        }
+
+    });
+}

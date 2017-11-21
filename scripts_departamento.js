b4750a5d83731a5037f6a68ff72c15fe82889ed0
ArkhamBat26 <serg.tello26@gmail.com>
2017-11-21 03:06:21 -0600

    AREA, DEPARTAMENTO (SUS PHP's Y EL SCRIPT)

--- scripts_departamento.js
+++ scripts_departamento.js
@@ -1,16 +1,88 @@
 $(document).ready(function(){
+    //Mostrar departamentos en la vista de departamentos
+    mostrarDepartamentos();
+
     //Funcion para agregar un nuevo depa
-    $("#form-agregarDepa").submit(function(data){
-        //Aqui pondría la función que registra a la base de datos, SU TUVIERAMOS UNA!
-        var nomDepa = $(this).find("input[name='nomDepa']").val();
-        var areaDepa =$(this).find("input[name='areaDepa']").val();
-        var str="Se dió de alta el departamento "+nomDepa;
+    $("#form-agregarDepa").submit(function(){
+        //Aqui pondría la función que registra a la base de datos, LA CUAL YA EXISTE
+        ////////////////////////////////////////DEPARTAMENTOS
+        var nombreDepa = $(this).find("input[name='nomDepa']").val();
+        var areaDepas =$(this).find("input[name='areaDepa']").val();
+        var seguir = false;
+        alert(nombreDepa+" "+areaDepas+" "+seguir);
+        $.ajax({
+            url:"php/departamentos.php",
+            method:"post",
+            data:{
+                operacion: 'A',
+                nombreDepa:nombreDepa
+            }
+        }).done(function(data){
+            if(data=="EXISTE")
+                Materialize.toast("¡Ya existe ese cliente!",2000,"rounded");
+            else if(data=="OK"){
+                seguir = true;
+            }
+            else
+                Materialize.toast("Error inesperado",2000,"rounded");
+        });
+
+
+        if(seguir){
+            ////////////////////////////////////////AREAS
+                console.log("Areas sin modificar: "+areaDepas);
+                //Debido a que pueden ser uno o varios departamentos separados por comas, primero verificamos que exista más de uno
+                //Si la función indexOf regresa -1, solo hay un área a registrar
+                if(areasDepas.indexOf(",")!=-1){
+                    //Creamos un arreglo con cada una de las áreas separadas por coma
+                    var areas = areaDepas.split(",");
+                    //Hacemos una iteración por cada área que se halla registrado. De esta manera se inserta cada una de las áreas
+                    for(var i = areas.length-1 ; i>=0 ; i--){
+                        $.ajax({
+                            url:"php/areas.php",
+                            method:"post",
+                            data:{
+                                operacion: 'A',
+                                nombreDepa:nombreDepa,
+                                area:area[i]
+                            }
+                        }).done(function(data){
+                                if(data=="OK"){
+                                Materialize.toast(str,2000,"rounded");
+                            }
+                            else
+                                Materialize.toast("Error inesperado",2000,"rounded");
+                        });
+                    }
+                }
+                //Solo hay un área y no necesita iterarse
+                else{
+                    $.ajax({
+                            url:"php/areas.php",
+                            method:"post",
+                            data:{
+                                operacion: 'A',
+                                nombreDepa:nombreDepa,
+                                area:areasDepas
+                            }
+                        }).done(function(data){
+                                if(data=="OK"){
+                                Materialize.toast(str,2000,"rounded");
+                            }
+                            else
+                                Materialize.toast("Error inesperado",2000,"rounded");
+                        });
+                }
+        }
+
+
         Materialize.toast(str,1500,'rounded');
 
         //Borrar datos y cerrar modal
         $(this).find("input[name='nomDepa']").val("");
         $(this).find("input[name='areaDepa']").val("");
         $("#agregarDepa").modal('close');
+        alert("fin");
         return false;
     });
 
@@ -43,7 +115,7 @@ $(document).ready(function(){
         return false;
     });
 
-    //Funcion para buscar un piso y eliminarlo
+    //Funcion para buscar un departamento y eliminarlo
     $("#form-eliminarDepa").submit(function(evt){
         var nomDepa = $(this).find("input[name='nomDepa']").val();
         if(Math.random()<0.5){
@@ -58,7 +130,7 @@ $(document).ready(function(){
         }
         return false;
     });
-    //Funcion para eliminar un piso
+    //Funcion para eliminar un departamento
     $("#btnEliminarDepa").click(function(evt){
         $("#confirmarEliminar").modal("close");
         //FUNCION PARA BORRAR EN LA DB
@@ -66,3 +138,25 @@ $(document).ready(function(){
         Materialize.toast(stg,2000,"rounded");
     });
 });
+
+function mostrarDepartamentos(){
+    $.ajax({
+        url:"php/departamentos.php",
+        method:"post",
+        data:{
+            operacion:'L'
+        }
+    }).done(function(data){
+        if(data=="[]")
+            $("#contenidoPrincipal").append("No hay departamentos por mostrar");
+        else{
+            var listaDepartamentos = JSON.parse(data);
+            var tabla = "";
+            for(var k in listaDepartamentos)
+                tabla+="<tr><td data-title='Nombre del depto.'>"+listaDepartamentos[k].nombre+"</td><td data-title='Áreas'>"+listaDepartamentos[k].area+"</td></tr>";
+            $("#contenidoPrincipal > div > table > tbody").html(tabla);
+        }
+
+    });
+}
+

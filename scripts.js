44a420fe81f9e53902bc1576651d6b50207e44f9
Jorge Garcia <14121463@tecmor.mx>
2017-11-21 00:34:10 -0600

    Boton de admin

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+validarLogin();
 $(document).ready(function(){
     $(".button-collapse").sideNav();
    $(".modal").modal({
@@ -17,3 +18,23 @@ $(document).ready(function(){
         format: 'dd/mm/yyyy'
     });
 });
+function validarLogin(){
+    $.ajax({//Validar la sesión
+        url:"php/usuario.php",
+        method:"post",
+        data:{
+            operacion:"S"
+        }
+    }).done(function(data){
+        if(data=="NOK"){
+            if(window.location.href.substr(window.location.href.length-10)!="index.html")
+                window.location.href="index.html";
+        }
+        else{
+            var usuario = JSON.parse(data);//usuario,nombre,rol
+            if(usuario[2]!="A"){
+                $(".btnAdmin").hide();
+            }
+        }
+    });
+}
